1c0f1720b8ddb80ee781e7b4a454c9287e6a92b8
theNonDangerousButInvisibleAndAnonymousCoder <thecat@thedog.themouse>
2023-05-03 22:25:46 -0300

    css tore locator

--- servicemode.py
+++ servicemode.py
@@ -3,6 +3,8 @@ class servicemodepage(directory):
   def __init__(self,title,params):
     self.title=title
     self.path="./store-locator"
+    self.set_css("")
+    self.add_css("servicemode.css")
     self.only_set_header_withthispath("headersignin.html")
     f=self.get_file("servicemode.html").read()
     self.set_content(f)
