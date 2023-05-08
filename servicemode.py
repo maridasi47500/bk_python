b1781e00488c3cec3259c2a1bc1e7fa35caa0eda
theNonDangerousButInvisibleAndAnonymousCoder <thecat@thedog.themouse>
2023-05-08 19:25:20 -0300

    css

--- servicemode.py
+++ servicemode.py
@@ -6,7 +6,7 @@ class servicemodepage(directory):
     self.set_css("")
     self.add_css("servicemode.css")
     self.only_set_header_withthispath("headersignin.html")
-    f=self.get_file("servicemode.html").read()
+    f=self.get_file("servicemode.html").read().decode("utf-8")
     self.set_content(f)
     print("fin de cod")
 
