ec3cceb9ef7828696025f4fd55e3d709b6a39e3f
theNonDangerousButInvisibleAndAnonymousCoder <thecat@thedog.themouse>
2023-06-05 22:14:24 -0300

    python bk

--- address.py
+++ address.py
@@ -16,7 +16,12 @@ class addresspage(directory):
     self.add_js_link("https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.3/leaflet.js")
     self.add_css_link("https://maps.locationiq.com/v2/libs/leaflet-geocoder/1.9.6/leaflet-geocoder-locationiq.min.css")
     self.add_js_link("https://maps.locationiq.com/v2/libs/leaflet-geocoder/1.9.6/leaflet-geocoder-locationiq.min.js")
+    #add marker
+    self.add_css_link("https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css")
+    self.add_js_link("https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js")
+    self.add_css("servicemode.css")
+    self.add_js("address.js")
 
-    a=self.get_file("./leaflet.js").read().encode('utf-8').replace('{GEOCODERAPIKEY}',os.environ['GEOCODERAPIKEY'])
+    #a=self.get_file("./leaflet.js").read().encode('utf-8').replace('{GEOCODERAPIKEY}',os.environ['GEOCODERAPIKEY'])
     self.set_content(f+a)
     print("fin de cod")
